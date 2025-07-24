3854a5b1984064acf4b36efd664dca4923337813
Krishna Kumar Agrahari <krishnaagrahari.16@gmail.com>
2025-07-25 00:26:42 +0530

    Errors done

--- portfolio/test/widget_test.dart
+++ portfolio/test/widget_test.dart
@@ -3,15 +3,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:portfolio/main.dart';
 
 void main() {
-
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-=======
   testWidgets('Portfolio app launches and shows Home screen', (
     WidgetTester tester,
   ) async {
     // Build the app and trigger a frame.
->>>>>>> e5befee38765cd046957a9638d611e84d48f52fc
     await tester.pumpWidget(const PortfolioApp());
 
     // Verify something from the Home screen is visible.
